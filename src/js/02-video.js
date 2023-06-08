a31265c1196088c9035f967dcd3ddb70da089642
Yaroslav337 <pelerovteatr@gmail.com>
2023-06-08 21:43:42 +0300

    update the 1st & 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+
+    localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video's duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
